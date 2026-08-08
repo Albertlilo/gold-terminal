5d9fc451a31eebc4b821faf43538b9ac6c89ed60
Albert Adjabeng <albert.adjabeng2@gmail.com>
2026-08-08 14:54:43 +0100

    Upgrade Gold Score and Home dashboard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
+const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 const fredRoutes = require("./routes/fredRoutes");
 
 require("dotenv").config();
 
 const usersRouters = require("./routes/usersRoutes");
-
-const express = require("express");
-const app = express();
-app.use(express.json());
 
 
 app.get("/api/health", (req, res) => {
